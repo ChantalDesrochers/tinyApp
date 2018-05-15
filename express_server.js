76825f35725af560b770f6432f0d9464296c19ee
Chantal <chandesrochers@gmail.com>
2018-05-15 21:11:09 +0000

    added random string functionality and post request on express_server view

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}))
@@ -10,6 +10,16 @@ var urlDatabase = {
  "b2xVn2": "http://www.lighthouselabs.ca",
  "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+var randomString = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 6; i++) {
+    randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return randomString;
+}
+
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -28,11 +38,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
   longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
