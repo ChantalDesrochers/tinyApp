b192b03d8b65336949b3067f330c6d0ad6482b2b
Chantal <chandesrochers@gmail.com>
2018-05-16 18:06:54 +0000

    added cookie tracking

--- express_server.js
+++ express_server.js
@@ -2,11 +2,15 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+
 
 app.set("view engine", "ejs"); //set view engine to ejs
 app.use(bodyParser.urlencoded({
   extended: false
 }))//added body parser
+app.use(cookieParser())
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -35,6 +39,7 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//READ display all urls
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase
@@ -42,10 +47,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//CREATE form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//CREATE post Route
 app.post("/urls", (req, res) => {
   var random = generateRandomString();
   urlDatabase[random] = req.body.longURL;
@@ -54,6 +61,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${random}`); // Respond with 'Ok' (we will replace this)
 });
 
+//READ specifc pages
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
@@ -62,6 +70,7 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//Redirect route
 app.get("/u/:shortURL", (req, res) => {
   console.log(urlDatabase[req.params.shortURL]);
   let longURL = urlDatabase[req.params.shortURL];
@@ -86,6 +95,16 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id]; //deleting the url in the database
   res.redirect("/urls");
 });
+
+//AUTHENTICATE (sort of) route
+app.post("/login", (req, res) => {
+var userInput = req.body.username;
+res.cookie("username", `${userInput}`);
+
+res.redirect("/urls");
+});
+
+
 
 
 app.listen(PORT, () => {
